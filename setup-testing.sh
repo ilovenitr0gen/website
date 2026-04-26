10b5c85310bd1ef50f05e661f609e6d96f5542b1
Caden <cadenhird@gmail.com>
2026-04-26 12:40:28 +0100

    commit

--- setup-testing.sh
+++ setup-testing.sh
@@ -1,3 +1,4 @@
 mkdir -p /www
+rm -r /www/site
 cp -r site /www/
 ln -sf "$(pwd)/nginx-testing.conf" /etc/nginx/nginx.conf
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 mkdir -p /www
+rm -r /www/site
 cp -r site /www/
 ln -sf "$(pwd)/nginx.conf" /etc/nginx/
